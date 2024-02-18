8fb9bc4eb0b9c6232710eb0c10876288e725d6be
alien365 <122644677+36524760@users.noreply.github.com>
2024-02-18 12:26:21 -0800

    feat(backend/apirest/user): add fields and constrains to User class

--- backend/apirest/src/main/java/com/mycomp/apirest/user/User.java
+++ backend/apirest/src/main/java/com/mycomp/apirest/user/User.java
@@ -1,14 +1,22 @@
 package com.mycomp.apirest.user;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
-@Table(name="users")
+@Table(
+  name="users",
+  uniqueConstraints = { 
+    @UniqueConstraint(name="email_unique", columnNames = "email")
+  }
+)
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
@@ -17,11 +25,36 @@ public class User {
   @Column(name="name")
   private String name;
 
+  @Column(name="email", nullable = false)
+  private String email;
 
+  @Column(name="birthdate")
+  private LocalDate birthdate;
 
-  public User(String name) {
+  public User(String name, String email) {
     this.name = name;
+    this.email = email;
   }
+
+  public User(){}
+
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public LocalDate getBirthdate() {
+    return birthdate;
+  }
+
+  public void setBirthdate(LocalDate birthdate) {
+    this.birthdate = birthdate;
+  }
+
 
   public Long getId() {
     return id;
