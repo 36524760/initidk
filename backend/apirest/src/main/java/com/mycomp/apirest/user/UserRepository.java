edcab032bdb9ac8628b704ed203eb11c481c0051
alien365 <122644677+36524760@users.noreply.github.com>
2024-02-18 12:27:33 -0800

    feat(backend/apirest/user): add emailExists method at UserRepository

--- backend/apirest/src/main/java/com/mycomp/apirest/user/UserRepository.java
+++ backend/apirest/src/main/java/com/mycomp/apirest/user/UserRepository.java
@@ -1,9 +1,21 @@
 package com.mycomp.apirest.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+  // Optional<User> findByEmail(String email);
+
+  @Query("" +
+    "SELECT CASE WHEN COUNT(u) > 0 THEN "+
+    "TRUE ELSE FALSE END " +
+    "FROM User u " +
+    "WHERE u.email = ?1"
+  )
+  Boolean emailExists(String email);
 
 }
