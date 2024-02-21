17817fad8c40e2de6328d0d91a986e460b2c913c
Sebastian Vidal <sebastianvr63@gmail.com>
2024-02-21 12:37:01 -0300

    feature(web): new configuration to make requests to localhost:8088.

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/:path*',
+                destination: 'http://localhost:8088/:path*',
+            },
+        ];
+    },
+};
 
 export default nextConfig;
