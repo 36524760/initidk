b87cd5b73ff98900601f65d9e31f14fac56ae67f
Sebastian Vidal <sebastianvr63@gmail.com>
2024-02-22 18:26:23 -0300

    hotfix(web/src/examples): Addiing key missing.

--- web/src/app/examples/page.tsx
+++ web/src/app/examples/page.tsx
@@ -1,24 +1,24 @@
 "use client";
 
-import TableExampleLayout from "./layouts/table-example-layout";
-import OptionsExampleLayout from "./layouts/options-example-layout";
-import ListExampleLayout from "./layouts/list-example-layout";
+import TableExampleLayout from './layouts/table-example-layout';
+import ListExampleLayout from './layouts/list-example-layout';
+import OptionsExampleLayout from './layouts/options-example-layout';
 
 export default function ExamplePage() {
   return (
     <div className="container mx-auto px-3 py-10">
       {/* Layout import */}
-      <TableExampleLayout></TableExampleLayout>
+      <TableExampleLayout key={1}></TableExampleLayout>
 
       <hr className="my-10 border-t border-gray-300 dark:border-gray-700" />
 
       {/* Layout import */}
-      <ListExampleLayout></ListExampleLayout>
+      <ListExampleLayout key={2}></ListExampleLayout>
 
       <hr className="my-10 border-t border-gray-300 dark:border-gray-700" />
 
       {/* Layout import */}
-      <OptionsExampleLayout></OptionsExampleLayout>
+      <OptionsExampleLayout key={3}></OptionsExampleLayout>
     </div>
   );
 }
